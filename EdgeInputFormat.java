5b6d96556b2a95eafa13146f5faae250bc0b201b
Himanshu Dahiya <2015csb1013@iitrpr.ac.in>
2019-02-01 11:35:12 +0530

    updated edgeInputFormat

--- EdgeInputFormat.java
+++ EdgeInputFormat.java
@@ -1,6 +1,35 @@
+package btp;
+
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.conf.LongConfOption;
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.log4j.Logger;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.edge.EdgeFactory;
+import org.apache.giraph.io.EdgeReader;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+//import org.apache.giraph.utils.ArrayWritable;
+import org.apache.hadoop.io.ArrayWritable;
 public class EdgeInputFormat extends
 org.apache.giraph.io.formats.TextEdgeInputFormat<LongWritable,
-LongWritable> {
+Text> {
 
 public EdgeInputFormat() {
 
@@ -32,18 +61,36 @@ Long.parseLong(line.toString().split(":")[1]));
 }
 
 
-protected LongWritable getValue(org.apache.hadoop.io.Text line) {
+protected Text getValue(org.apache.hadoop.io.Text line) {
+	String str = line.toString().split(":")[2];
+	Text t1 = new Text(str);	
+	 System.out.println(t1);
+	return t1;
+	
+	
+//	String arr[] = str.split(",");
+//	int len = arr.length;
+//	LongWritable[] array = new LongWritable[len];
+//	
+//	for(int i=0;i<len;i++) {
+//		array[i] =  new LongWritable( Long.parseLong(arr[i]));
+//	}
+	
+	//ArrayWritable aw = new ArrayWritable((Class<? extends Writable>) EdgeInputFormat.class,array);
+	//aw.set(array);
 
-return new LongWritable(
+	
+	//Arrays.stream(str.split(",")).map(String::trim);
+	//.mapToInt(Integer::parseInt).toArray();
 
-Long.parseLong(line.toString().split(":")[2]));
+	
 
 }
 
 }
 
 
-public EdgeReader<LongWritable, LongWritable> createEdgeReader(
+public EdgeReader<LongWritable, Text> createEdgeReader(
 
 InputSplit split, TaskAttemptContext context) throws IOException {
 
